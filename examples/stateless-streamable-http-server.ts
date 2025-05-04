817406d56cfedbe989b9da356c640f4544bd2a4f
Kshitij Chauhan <kshitijchauhan1105@outlook.com>
2025-05-04 12:59:35 +0100

    Update stateless example

--- examples/stateless-streamable-http-server.ts
+++ examples/stateless-streamable-http-server.ts
@@ -1,48 +1,47 @@
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
-import type { StreamableHTTPServerTransport } from "@modelcontextprotocol/sdk/server/streamableHttp.d.ts";
 import { fastify } from "fastify";
-import { Sessions, streamableHttp } from "../src";
+import { streamableHttp } from "../src";
 
-const mcpServer = new McpServer({
-  name: "stateless-streamable-http-server",
-  version: "0.0.1",
-});
-
-mcpServer.tool("greet", () => {
-  return {
-    content: [{ type: "text", text: "Hello, world!" }],
-  };
-});
-
-async function main() {
-  const app = fastify({
-    logger: {
-      level: "error",
-      transport: {
-        target: "pino-pretty",
-        options: {
-          translateTime: "HH:MM:ss Z",
-          ignore: "pid,hostname",
-        },
+const app = fastify({
+  logger: {
+    level: "error",
+    transport: {
+      target: "pino-pretty",
+      options: {
+        translateTime: "HH:MM:ss Z",
+        ignore: "pid,hostname",
       },
     },
-  });
-
-  app.register(streamableHttp, {
-    stateful: true,
-    mcpEndpoint: "/mcp",
-    createServer: () => mcpServer.server,
-    sessions: new Sessions<StreamableHTTPServerTransport>(),
-  });
-
-  await app.listen({
-    port: 3000,
-  });
-
-  console.log("Server is running on port 3000");
-}
-
-main().catch((err) => {
-  console.error(err);
-  process.exit(1);
+  },
 });
+
+app.register(streamableHttp, {
+  stateful: false,
+  mcpEndpoint: "/mcp",
+  createServer: () => {
+    const mcpServer = new McpServer({
+      name: "stateless-streamable-http-server",
+      version: "0.0.1",
+    });
+
+    mcpServer.tool("greet", () => {
+      return {
+        content: [{ type: "text", text: "Hello, world!" }],
+      };
+    });
+
+    return mcpServer.server;
+  },
+});
+
+app
+  .listen({
+    port: 3000,
+  })
+  .then(() => {
+    console.log("Server is running on port 3000");
+  })
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
